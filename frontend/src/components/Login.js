b5430bf0dbbca328f39848491f32b960c1a4d3d8
Sergio Gonzalez <gsergio96@knights.ucf.edu>
2020-10-28 01:22:49 -0400

    server.js with working login API

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,47 +1,74 @@
 import React, { useState } from 'react';
 
-function Login() {
-    var loginName, loginPassword;
+function Login()
+{
+    //Creates the variables for the inputs
+    var loginName;
+    var loginPassword;
+  
+    const [message,setMessage] = useState('');
 
-    const [message, setMessage] = useState('');
+    //When we try to login
+    const doLogin = async event => 
+    {
+        event.preventDefault();
 
-    const doLogin = async event => {     
-        
-        
-        event.preventDefault();        
-        var obj = {username:loginName.value,password:loginPassword.value};        
-        var js = JSON.stringify(obj);        
-        
-        try {                
-            const response = await fetch('http://localhost:5000/api/login', {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});            
+        var obj = {login:loginName.value,password:loginPassword.value};
+        var js = JSON.stringify(obj);
+        alert (js);
+        try
+        {   
+            //- fetch() is what starts the request. Requires a resource (the URL)
+            //  as well as options which are the method: 'POST' etc. stuff
+
+            //- Since doLogin is asynchronous, the await keyword pauses the function
+            //  until the request is complete
+
+            //- Note that it says body:js. js is the json object that has the login and
+            //  password so from there we are sending the login and password to the API
+            const response = await fetch('http://localhost:5000/api/login',
+              {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+
+            //now that we have the result of what was sent back from the api,
+            //we now parse it
             var res = JSON.parse(await response.text());
-            if( res.id <= 0 ) {                
-                setMessage('User/Password combination incorrect');            
-            } else {                
-                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id};           
-                localStorage.setItem('user_data', JSON.stringify(user));                
-                setMessage('');               
-                window.location.href = '/home';            
-            }        
-        } catch(e) {            
-            alert(e.toString());            
-            return;        
-        }        
-    }; 
+       
+            if( res.flag <= 0 )
+            {
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                //create a user variable and stores that variable
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
 
-    return (
-        <div id="loginDiv">
+                //no error so empty string
+                //*******dont know what '/cards' take you*****
+                setMessage('');
+                window.location.href = '/cards';
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
+    };
 
-            <form onSubmit={ doLogin }>
-                <span id="inner-title">Username: test <br /> Password: test</span><br />
-                <input type="text" id="loginName" placeholder="Username"   ref={(c) => loginName = c} /><br />
-                <input type="password" id="loginPassword" placeholder="Password"   ref={(c) => loginPassword = c} /><br />
-                <input type="submit" id="loginButton" class="buttons" value="Login" onClick={doLogin} />       
-            </form>
-            <a href="/register">Click Here To Register</a><br />
-            <span id="loginResult">{message}</span>
-        </div>
+
+    return(
+      <div id="loginDiv">
+        <form onSubmit={doLogin}>
+        <span id="inner-title">PLEASE LOG IN</span><br />
+        <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
+        <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
+        <input type="submit" id="loginButton" class="buttons" value = "Do It"
+          onClick={doLogin} />
+        </form>
+        <span id="loginResult">{message}</span>
+     </div>
     );
-}
+};
 
-export default Login; 
+export default Login;
--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://mwaldrep26:mwaldrep@lpcluster.awkzh.mongodb.net/G26_Database?retryWrites=true&w=majority';
 
+const client = new MongoClient(url);
+client.connect();
 
 app.use((req, res, next) => {  
     res.setHeader('Access-Control-Allow-Origin', '*');  
@@ -15,23 +19,66 @@ app.use((req, res, next) => {
     next();
 });
 
+app.listen(5000); // start Node + Express server on port 5000
 
-app.post('/api/login', async (req, res, next) => {  
-    // incoming: login, password  
-    // outgoing: id, firstName, lastName, error  
-    var error = '';  
-    const { username, password } = req.body;  
-    var id = -1;  
-    var fn = '';  
-    var ln = '';  
-    if( username.toLowerCase() == 'test' && password == 'test' )  {
-        id = 1;    
-        fn = 'testfirst';    
-        ln = 'testlast';  
-    }  else  {    
-        error = 'Invalid user name/password';  
-    }  
-    var ret = { id:id, firstName:fn, lastName:ln, error:error};  
+// app.post('/api/login', async (req, res, next) => {  
+//     // incoming: login, password  
+//     // outgoing: id, firstName, lastName, error  
+//     var error = '';  
+//     const { username, password } = req.body;  
+//     var id = -1;  
+//     var fn = '';  
+//     var ln = '';  
+//     if( username.toLowerCase() == 'test' && password == 'test' )  {
+//         id = 1;    
+//         fn = 'testfirst';    
+//         ln = 'testlast';  
+//     }  else  {    
+//         error = 'Invalid user name/password';  
+//     }  
+//     var ret = { id:id, firstName:fn, lastName:ln, error:error};  
+//     res.status(200).json(ret);
+// });
+
+app.post('/api/login', async (req, res, next) => 
+{
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+
+    // Create empty error variable
+    var error = '';
+
+    //req is what was sent over from the login function
+    //which is login and password
+    const { login, password } = req.body;
+
+    //this is just the API so i dont have the server code here
+    //but client has the all the info for the MongoDB database
+    //and allows us to connect to it
+
+    //So db.collection('Users') accesses the collection in the database
+    //called Users and attempts to find a user with the login and password
+    //that was sent to the API then converts all the info on that user to
+    //an array
+    const db = client.db();
+    const results = await db.collection('User').find({Username:login,Password:password}).toArray();
+
+    //everything down here is pretty self explanatory
+    var flag = -1;
+    var email = '';
+    var validated = -1
+
+    if( results.length > 0 )
+    {
+        flag = 1;
+        email = results[0].Email;
+        validated = results[0].Validated;
+    }
+
+    //here we are returning what we got back to the function
+    //So we're returning an id, a firstname, lastname and an
+    //error if any
+    var ret = {Flag:flag, Email:email, Validated:validated, error:''};
     res.status(200).json(ret);
 });
 
