137726fab16f66e1d11dbb4a5b92f73713dd571a
spadediamond <spadediamond1@gmail.com>
2020-10-28 02:52:42 -0400

    Changed login.js to work with API

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -9,15 +9,16 @@ function Login() {
         
         
         event.preventDefault();        
-        var obj = {username:loginName.value,password:loginPassword.value};        
+        var obj = {login:loginName.value,password:loginPassword.value};        
         var js = JSON.stringify(obj);        
         
         try {                
             const response = await fetch('http://localhost:5000/api/login', {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});            
             var res = JSON.parse(await response.text());
-            if( res.id <= 0 ) {                
+            if( res.Flag <= 0 ) {                
                 setMessage('User/Password combination incorrect');            
-            } else {                
+            } else {      
+                // Temporary user display          
                 var user = {firstName:res.firstName,lastName:res.lastName,id:res.id};           
                 localStorage.setItem('user_data', JSON.stringify(user));                
                 setMessage('');               
@@ -33,7 +34,7 @@ function Login() {
         <div id="loginDiv">
 
             <form onSubmit={ doLogin }>
-                <span id="inner-title">Username: test <br /> Password: test</span><br />
+                <span id="inner-title">Username: Test <br /> Password: abc123</span><br />
                 <input type="text" id="loginName" placeholder="Username"   ref={(c) => loginName = c} /><br />
                 <input type="password" id="loginPassword" placeholder="Password"   ref={(c) => loginPassword = c} /><br />
                 <input type="submit" id="loginButton" class="buttons" value="Login" onClick={doLogin} />       
