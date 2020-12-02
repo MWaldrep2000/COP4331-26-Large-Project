a8aeb5bd9e693291c9d7cf2657357409511131a8
JoelMarq24 <joel.marquez@knights.ucf.edu>
2020-12-01 20:01:33 -0500

    Finished issuePage, but you can only look at replies

---
+++ startup_namer/lib/issueReplies.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class issueReplies{
+  var replyList;
+  String error;
+
+  issueReplies({this.replyList, this.error});
+
+  // factory keyword is used when the constructor doesn't always create an object
+  factory issueReplies.fromJson(Map<String, dynamic> json) {
+    return issueReplies(
+        replyList : json['Results'],
+        error: json['Error']
+    );
+  }
+}
+
+Future<issueReplies> fetchissueRepliesResults(String issueID) async{
+  final http.Response response = await http.post(
+    'https://hivemindg26.herokuapp.com/api/readReplies',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: jsonEncode(<dynamic, dynamic>{
+      'issueID': issueID
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    // print(response.body);
+    return issueReplies.fromJson(jsonDecode(response.body));
+  }
+  else{
+    throw Exception('Failed to load');
+  }
+}
--- startup_namer/lib/pages/Issue.dart
+++ startup_namer/lib/pages/Issue.dart
@@ -10,6 +10,7 @@ import 'package:startup_namer/pages/HomePage.dart';
 import 'package:startup_namer/pages/Login.dart';
 import 'package:startup_namer/getGroups.dart';
 import 'package:startup_namer/joinGroup.dart';
+import 'package:startup_namer/pages/MyIssues.dart';
 import 'package:startup_namer/searchGroups.dart';
 import 'package:startup_namer/getMyIssues.dart';
 import 'package:startup_namer/pages/PasswordRecovery.dart';
@@ -17,6 +18,8 @@ import 'package:localstorage/localstorage.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+import '../issueReplies.dart';
 
 
 class Issue extends StatefulWidget {
@@ -26,8 +29,8 @@ class Issue extends StatefulWidget {
 
 class _IssueState extends State<Issue> {
 
-  Future<getMyIssues> _getMyIssuesResults;
-  getMyIssues currentIssues;
+  Future<issueReplies> _issueRepliesResults;
+  issueReplies currentIssues;
 
   final LocalStorage storage = new LocalStorage('data');
 
@@ -36,10 +39,10 @@ class _IssueState extends State<Issue> {
   Widget build(BuildContext context) {
 
     // Initially gets all of the groups in the database
-    _getMyIssuesResults = fetchMyIssuesResults(storage.getItem('User'));
-    _getMyIssuesResults.then((temp) {
+    _issueRepliesResults = fetchissueRepliesResults(storage.getItem('IssueID'));
+    _issueRepliesResults.then((temp) {
       print("helloiiieieie");
-      currentIssues = temp;
+      print(temp.replyList.runtimeType);
 
       //   print(currentIssues.issuesList);
       //   // print(currentGroups.groupList);
@@ -63,7 +66,7 @@ class _IssueState extends State<Issue> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => HomePage()),
+                      MaterialPageRoute(builder: (context) => MyIssues()),
                     );
                   }
               ),
@@ -101,73 +104,105 @@ class _IssueState extends State<Issue> {
                       child: Column(
                           children: <Widget>[
                             Padding(
-                                padding: EdgeInsets.only(top:20)
+                                padding: EdgeInsets.only(top:4)
                             ),
 
                             // FutureBuilder(
 
                             // ),
-                            Padding(
-                              padding: EdgeInsets.only(top:20),
+                            // Padding(
+                            //   padding: EdgeInsets.only(top:10),
+                            // ),
+                            Card(
+                              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 10.0),
+                              child: Container(
+                                width: 600,
+                                height: 80,
+                                padding: EdgeInsets.all(10),
+                                child: Column(
+                                  children: <Widget>[
+                                    // Padding()
+                                    Text(
+                                      'Issue: ${storage.getItem('CurrentIssueInfo')['Topic']}',
+                                       overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      'Posted By: ${storage.getItem('CurrentIssueInfo')['Username']}',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                    Text(
+                                      'Description: ${storage.getItem('CurrentIssueInfo')['Description']}',
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ],
+                                ),
+                              )
                             ),
 
                             SingleChildScrollView(
                               child: FutureBuilder(
-                                  future: _getMyIssuesResults,
+                                  future: _issueRepliesResults,
                                   builder: (BuildContext context, AsyncSnapshot snapshot) {
                                     List<Widget> children;
                                     print('yerrrrrr');
-                                    if (snapshot.connectionState == ConnectionState.done && snapshot.data.issuesList != null) {
+                                    if (snapshot.connectionState == ConnectionState.done && snapshot.data.replyList != null) {
                                       List<Widget> cardWidgets = [];
                                       print('made it past the nullllllllll');
-                                      print(snapshot.data.issuesList);
+                                      print(snapshot.data.replyList);
                                       if(snapshot.data != null){
-                                        for (int i = 0; i < snapshot.data.issuesList.length; i++) {
+                                        for (int i = 0; i < snapshot.data.replyList.length; i++) {
                                           cardWidgets.add(Card(
-                                              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
+                                              margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
                                               child: Padding(
                                                 padding: const EdgeInsets.all(16.0),
                                                 child: Row(
                                                     children: <Widget> [
                                                       Container(
-                                                        width: 200,
+                                                        width: 100,
                                                         child: Text(
-                                                          snapshot.data.issuesList[i]['Topic'],
+                                                          "User: ${snapshot.data.replyList[i]['Author']}",
                                                           overflow: TextOverflow.ellipsis,
                                                         ),
                                                       ),
                                                       Padding(
                                                           padding: EdgeInsets.only(left:10)
                                                       ),
-                                                      RaisedButton(
-                                                        elevation: 5.0,
-                                                        onPressed: () {
-                                                          print("Something stupid");
-                                                          Navigator.push(
-                                                            context,
-                                                            MaterialPageRoute(builder: (context) => Issue()),
-                                                          );
-                                                          // print(snapshot.data[1].groupList[i]['_id']);
-                                                          // _getJoinGroupResults = fetchJoinResults(storage.getItem('ID'), snapshot.data[0].groupList[i]['_id']);
-                                                          // _getJoinGroupResults.then((JoinGroupResult){
-                                                          //   print("hahahahahahaha");
-                                                          //   print(storage.getItem('ID'));
-                                                          //   print(snapshot.data[0].groupList[i]['_id']);
-                                                          //   Fluttertoast.showToast(msg: JoinGroupResult.error);
-                                                          // });
-                                                        },
-                                                        shape: RoundedRectangleBorder(
-                                                          borderRadius: BorderRadius.circular(10.0),
-                                                        ),
-                                                        color: Colors.tealAccent,
+                                                      Container(
+                                                        width: 200,
                                                         child: Text(
-                                                          'View Issue',
-                                                          style: TextStyle(
-                                                            color: Colors.teal[400],
-                                                            letterSpacing: 2,
-                                                          ),
+                                                          snapshot.data.replyList[i]['Reply'],
+                                                          overflow: TextOverflow.ellipsis,
                                                         ),
-                                                      )
+                                                      ),
+                                                      // RaisedButton(
+                                                      //   elevation: 5.0,
+                                                      //   onPressed: () {
+                                                      //     print("Something stupid");
+                                                      //     Navigator.push(
+                                                      //       context,
+                                                      //       MaterialPageRoute(builder: (context) => Issue()),
+                                                      //     );
+                                                      //     // print(snapshot.data[1].groupList[i]['_id']);
+                                                      //     // _getJoinGroupResults = fetchJoinResults(storage.getItem('ID'), snapshot.data[0].groupList[i]['_id']);
+                                                      //     // _getJoinGroupResults.then((JoinGroupResult){
+                                                      //     //   print("hahahahahahaha");
+                                                      //     //   print(storage.getItem('ID'));
+                                                      //     //   print(snapshot.data[0].groupList[i]['_id']);
+                                                      //     //   Fluttertoast.showToast(msg: JoinGroupResult.error);
+                                                      //     // });
+                                                      //   },
+                                                      //   shape: RoundedRectangleBorder(
+                                                      //     borderRadius: BorderRadius.circular(10.0),
+                                                      //   ),
+                                                      //   color: Colors.tealAccent,
+                                                      //   child: Text(
+                                                      //     'View Issue',
+                                                      //     style: TextStyle(
+                                                      //       color: Colors.teal[400],
+                                                      //       letterSpacing: 2,
+                                                      //     ),
+                                                      //   ),
+                                                      // )
                                                     ]
                                                 ),
                                               )
@@ -198,7 +233,7 @@ class _IssueState extends State<Issue> {
                                     );
                                   }
                               ),
-                            )
+                            ),
                           ]
                       )
                   ),
